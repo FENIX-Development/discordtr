61e89629fd4198faaa3304228e05fd1bdee4c3cf
Discord.PY Türkiye <62030752+FENIX-Development@users.noreply.github.com>
2020-03-19 18:32:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,25 +2,23 @@ from distutils.core import setup
 setup(
   name = 'discordtr',         
   packages = ['discordtr'],  
-  version = '0.3',      
+  version = '0.4',      
   license='MIT',       
   description = 'developed by fenix-development',  
-  author = 'berke',                  
+  author = 'JaimiTR',                  
   author_email = 'jaimitr14@gmail.com',     
   url = 'https://github.com/FENIX-Development/discordtr',  
-  download_url = 'https://github.com/FENIX-Development/discordtr/archive/v_03.tar.gz',   
+  download_url = 'https://github.com/FENIX-Development/discordtr/archive/v_04.tar.gz',   
   keywords = ['discordtr', 'fenixdev', 'discord-tr'],  
   install_requires=[           
-          'discordtr'
+          'discordtr',
+          'komutlar'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers', 
+    'Topic :: Build Tools',
+    'License :: MIT License',
+    'Programming Language :: Python :: 3.8',
   ],
 )
