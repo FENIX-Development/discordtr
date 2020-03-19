c45c4925a0a263c63901c62d7d89e9d7aafc9fc3
Discord.PY Türkiye <62030752+FENIX-Development@users.noreply.github.com>
2020-03-19 17:59:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'discordtr',         
   packages = ['discordtr'],  
-  version = '0.2',      
+  version = '0.3',      
   license='MIT',       
   description = 'developed by fenix-development',  
   author = 'berke',                  
   author_email = 'jaimitr14@gmail.com',     
   url = 'https://github.com/FENIX-Development/discordtr',  
-  download_url = 'https://github.com/FENIX-Development/discordtr/archive/v_02.tar.gz',   
+  download_url = 'https://github.com/FENIX-Development/discordtr/archive/v_03.tar.gz',   
   keywords = ['discordtr', 'fenixdev', 'discord-tr'],  
   install_requires=[           
           'discordtr'
