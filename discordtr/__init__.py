cbcc4f0a165217d75721787b40d230c8ebfad38a
Discord.PY Türkiye <62030752+FENIX-Development@users.noreply.github.com>
2020-03-21 17:06:47 +0000

    Create __init__.py

---
+++ discordtr/__init__.py
@@ -0,0 +1,22 @@
+__title__ = 'discordtr'
+__author__ = 'JaimiTR'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019-2020 JaimiTR'
+__version__ = '0.9'
+
+from collections import namedtuple
+from .komutlar import Bilgi, Emoji, Bot
+import logging
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=1, minor=3, micro=2, releaselevel='final', serial=0)
+
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
